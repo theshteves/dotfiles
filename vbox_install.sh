246e3be66be4ed24e1a2330e0da5a86e9c3dbe0c
theshteves <stevenkneiser@gmail.com>
2024-02-26 00:42:34 -0500

    Fixed broken vbox_install setup paths & made all absolute

--- vbox_install.sh
+++ vbox_install.sh
@@ -9,7 +9,7 @@
 # Pre-installation
 # nix-shell -p git parted home-manager # gnumake vim
 # git clone https://github.com/theshteves/dotfiles.git
-# cd dotfiles/nixos.config
+# cd dotfiles
 # chmod +x vbox_install.sh
 # sudo ./vbox_install.sh
 
@@ -39,13 +39,14 @@ swapon /dev/sda3
 
 ## Configuration
 #nixos-generate-config --root /mnt
+DOTFILES="/home/nixos/dotfiles"
 mkdir -p /mnt/etc/nixos
-cp ./nixos/* /mnt/etc/nixos
+cp $DOTFILES/nixos.config/nixos/* /mnt/etc/nixos
 nixos-install # --flake "github:theshteves/dotfiles#vbox"
 
-cp -r ~/dotfiles /mnt/home/bruh/dotfiles
+cp -r $DOTFILES /mnt/home/bruh/dotfiles
 mkdir -p /mnt/home/bruh/.config
 ln -sfn /mnt/home/bruh/dotfiles/nixos.config/* /mnt/home/bruh/.config  #TODO: careful of pre-existing .config
 #reboot
 
-#home-manager switch --flake ~/dotfiles/nixos.config#vbox
+#home-manager switch --flake /mnt/home/bruh/dotfiles/nixos.config#vbox --extra-experimental-features "nix-command flakes"
